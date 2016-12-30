7fc77a0a9efcd33047846eddb5f9fe703a282b31
mji <marek.jagielski@gmail.com>
2016-12-30 01:56:04 +0100

    progress

--- R/parser.R
+++ R/parser.R
@@ -7,3 +7,178 @@ load <- function(path, module_name=NA, include_dirs=NA) {
   thrift = parse(path, module_name, include_dirs=include_dirs)
   return(thrift)
 }
+
+LITERALS = ':;,=*{}()<>[]'
+
+THRIFT_RESERVED_KEYWORDS = c(
+  'BEGIN',
+  'END',
+  '__CLASS__',
+  '__DIR__',
+  '__FILE__',
+  '__FUNCTION__',
+  '__LINE__',
+  '__METHOD__',
+  '__NAMESPACE__',
+  'abstract',
+  'alias',
+  'and',
+  'args',
+  'as',
+  'assert',
+  'begin',
+  'break',
+  'case',
+  'catch',
+  'class',
+  'clone',
+  'continue',
+  'declare',
+  'def',
+  'default',
+  'del',
+  'delete',
+  'do',
+  'dynamic',
+  'elif',
+  'else',
+  'elseif',
+  'elsif',
+  'end',
+  'enddeclare',
+  'endfor',
+  'endforeach',
+  'endif',
+  'endswitch',
+  'endwhile',
+  'ensure',
+  'except',
+  'exec',
+  'finally',
+  'float',
+  'for',
+  'foreach',
+  'function',
+  'global',
+  'goto',
+  'if',
+  'implements',
+  'import',
+  'in',
+  'inline',
+  'instanceof',
+  'interface',
+  'is',
+  'lambda',
+  'module',
+  'native',
+  'new',
+  'next',
+  'nil',
+  'not',
+  'or',
+  'pass',
+  'public',
+  'print',
+  'private',
+  'protected',
+  'public',
+  'raise',
+  'redo',
+  'rescue',
+  'retry',
+  'register',
+  'return',
+  'self',
+  'sizeof',
+  'static',
+  'super',
+  'switch',
+  'synchronized',
+  'then',
+  'this',
+  'throw',
+  'transient',
+  'try',
+  'undef',
+  'union',
+  'unless',
+  'unsigned',
+  'until',
+  'use',
+  'var',
+  'virtual',
+  'volatile',
+  'when',
+  'while',
+  'with',
+  'xor',
+  'yield'
+)
+
+KEYWORDS = c(
+  'namespace',
+  'include',
+  'void',
+  'bool',
+  'byte',
+  'i16',
+  'i32',
+  'i64',
+  'double',
+  'string',
+  'binary',
+  'map',
+  'list',
+  'set',
+  'oneway',
+  'typedef',
+  'struct',
+  'union',
+  'exception',
+  'extends',
+  'throws',
+  'service',
+  'enum',
+  'const',
+  'required',
+  'optional'
+)
+
+TOKENS = c(c(
+  'BOOLCONSTANT',
+  'INTCONSTANT',
+  'DUBCONSTANT',
+  'LITERAL',
+  'IDENTIFIER'),
+  toupper(KEYWORDS)
+)
+
+Lexer <- R6Class("Lexer",
+  public = list(
+    tokens = TOKENS,
+    literals = LITERALS,
+    t_ignore = " \t\r",
+    t_error = function(t) {
+      stop(sprintf('Illegal characher %s at line %d', t$value[1], t$lineno))
+    },
+    t_newline = function(re='\\n+', t) {
+      t$lexer$lineno <- t$lexer$lineno + nchar(t$value)
+      return(NULL)
+    },
+    t_ignore_SILLYCOMM = function(re='\\/\\*\\**\\*\\/', t) {
+      t$lexer$lineno <- t$lexer$lineno + lengths(regmatches(t$value, gregexpr("a", t$value)))
+      return(NULL)
+    },
+    t_ignore_MULTICOMM = function(re='\\/\\*[^*]\\/*([^*/]|[^*]\\/|\\*[^/])*\\**\\*\\/', t) {
+      t$lexer$lineno <- t$lexer$lineno + lengths(regmatches(t$value, gregexpr("a", t$value)))
+      return(NULL)
+    },
+    t_ignore_DOCTEXT = function(re='\\/\\*\\*([^*/]|[^*]\\/|\\*[^/])*\\**\\*\\/', t) {
+      t$lexer$lineno <- t$lexer$lineno + lengths(regmatches(t$value, gregexpr("a", t$value)))
+      return(NULL)
+    },
+    t_ignore_UNIXCOMMENT = '\\#[^\\n]*',
+    t_ignore_COMMENT = '\\/\\/[^\\n]*'
+  )
+)
