6859edc0520f7d469875b7b805df321ecd5a9383
mji <marek.jagielski@gmail.com>
2017-01-05 20:42:38 +0100

    progress

--- R/parser.R
+++ R/parser.R
@@ -179,6 +179,60 @@ Lexer <- R6Class("Lexer",
       return(NULL)
     },
     t_ignore_UNIXCOMMENT = '\\#[^\\n]*',
-    t_ignore_COMMENT = '\\/\\/[^\\n]*'
+    t_ignore_COMMENT = '\\/\\/[^\\n]*',
+    t_BOOLCONSTANT = function(re='true|false', t) {
+      t$value <- t$value == 'true'
+      return(t)
+    },
+    t_DUBCONSTANT = function(re='-?\\d+\\.\\d*(e-?\\d+)?', t) {
+      t$value <- as.numeric(t$value)
+      return(t)
+    },
+    t_HEXCONSTANT = function(re='0x[0-9A-Fa-f]+', t) {
+      t$value <- strtoi(t$value)
+      t$type <- 'INTCONSTANT'
+      return(t)
+    },
+    t_INTCONSTANT = function(re='[+-]?[0-9]+', t) {
+      t$value <- strtoi(t$value)
+      return(t)
+    },
+    t_LITERAL = function(re='(\\"([^\\\n]|(\\.))*?\")|\'([^\\\n]|(\\.))*?\'', t) {
+      s <- tail(head(t, -1), -1)
+      maps <- new.env(hash=TRUE)
+      maps['t']  <- '\t'
+      maps['r']  <- '\r'
+      maps['n']  <- '\n'
+      maps['\\'] <- '\\'
+      maps['\''] <- '\''
+      maps['"']  <- '\"'
+      
+      i <- 1
+      length <- length(s)
+      val <- ''
+      while(i < length) {
+        if(s[[i]] == '\\') {
+          i <- i + 1
+          if(s[[i]] %in% maps) val <- val + maps[[s[[i]]]]
+          else {
+            msg <- sprintf('Unexcepted escaping characher: %s', s[[i]])
+            stop(msg)
+          }
+        } else val <- val + s[[i]]
+        i <- i + 1
+     }
+    
+      t$value <- val
+      return(t)
+    },
+    t_IDENTIFIER = function(re='[a-zA-Z_](\\.[a-zA-Z_0-9]|[a-zA-Z_0-9])*', t) {
+      if(t$value %in% keywords) {
+        t$type <- toupper(t$value)
+        return(t)
+      }
+      if(t$value %in% thrift_reserved_keywords)
+        stop(sprintf('Cannot use reserved language keyword: %s at line %d', t$value, t$lineno))
+      return(t)
+    }
   )
 )
