305c50c0d71bb8493b54b3268e379a03a6ae18d7
mji <marek.jagielski@gmail.com>
2017-01-06 14:48:46 +0100

    introduce tests

---
+++ tests/testthat.R
@@ -0,0 +1,6 @@
+#! /usr/bin/env Rscript
+
+library(testthat)
+library(thriftr)
+
+test_check("thriftr")
---
+++ tests/testthat/test.test_type.R
@@ -0,0 +1,11 @@
+#! /usr/bin/env Rscript
+
+library(testthat)
+library(thriftr)
+
+context("type")
+
+test_that("test_set", {
+  s <- thriftr_load("type.thrift")
+#  assert s.Set.thrift_spec == {1: (TType.SET, "a_set", TType.STRING, True)}
+})
