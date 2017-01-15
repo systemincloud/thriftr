11a91ce8b86b7db182ca33107c7bcc67574c9200
mji <marek.jagielski@gmail.com>
2017-01-15 23:52:23 +0100

    test.test_parser

--- tests/testthat/test.test_parser.R
+++ tests/testthat/test.test_parser.R
@@ -24,7 +24,93 @@ test_that("test_constants", {
   expect_that(thrift$tmap1[['key']],   equals('val'))
   expect_that(thrift$tmap2[['key']],   equals(32))
   expect_that(thrift$my_country,       equals(4))
+  # TODO
 #  expect_that(thrift$tom,              equals(thrift$Person$new(name='tom')))
 #  expect_that(thrift$country_map['1'], equals('US'))
 })
 
+test_that("test_include", {
+  # TODO
+  #thrift <- thriftr_load("parser-cases/include.thrift")
+  #expect_that(thrift$datetime, equals(1422009523))
+})
+
+test_that("test_tutorial", {
+  # TODO
+})
+
+test_that("test_e_type_error", {
+  # TODO
+})
+
+test_that("test_value_ref", {
+  # TODO
+})
+
+test_that("test_type_ref", {
+  # TODO
+})
+
+test_that("test_e_value_ref", {
+  # TODO
+})
+
+test_that("test_enums", {
+  # TODO
+})
+
+test_that("test_structs", {
+  # TODO
+})
+
+test_that("test_e_structs", {
+  # TODO
+})
+
+test_that("test_service", {
+  # TODO
+})
+
+test_that("test_service_extends", {
+  # TODO
+})
+
+test_that("test_e_service_extends", {
+  # TODO
+})
+
+test_that("test_e_dead_include", {
+  # TODO
+})
+
+test_that("test_e_grammer_error_at_eof", {
+  # TODO
+})
+
+test_that("test_e_use_thrift_reserved_keywords", {
+  # TODO
+})
+
+test_that("test_e_duplicate_field_id_or_name", {
+  # TODO
+})
+
+test_that("test_thrift_meta", {
+  # TODO
+})
+
+test_that("test_load_fp", {
+  # TODO
+})
+
+test_that("test_e_load_fp", {
+  # TODO
+})
+
+test_that("test_recursive_union", {
+  # TODO
+})
+
+test_that("test_issue_215", {
+  # TODO
+})
