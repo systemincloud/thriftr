c4854543554c0957405e07180fc8f5a21338a611
Marek Jagielski <marek.jagielski@gmail.com>
2018-08-02 22:55:44 +0200

    transport_socket

---
+++ R/transport_socket.R
@@ -0,0 +1,100 @@
+# MIT License
+#
+# Copyright (c) 2016 System in Cloud - Marek Jagielski
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#    
+#    The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+#' TSocket
+#'
+#' Socket implementation for client side.
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @format An \code{\link{R6Class}} generator object
+#'
+#' @export
+TSocket <- R6Class("TSocket",
+  public = list(
+    host = NA,
+    port = NA,
+    server = NA,
+    sock = NA,
+    initialize = function(
+        host,
+        port,
+        server = FALSE) {
+        self$host <- host
+        self$port <- port
+        self$server <- server
+    },
+    open = function() {
+      private$init_sock()
+    },
+    read = function(sz) {
+      readBin(self$sock, raw(), sz)
+    },
+    write = function(buff) {
+      writeBin(buff, self$sock)
+    },
+    close = function() {
+      close(self$sock)
+    }
+  ),
+  private = list(
+    init_sock = function() {
+      self$sock <- socketConnection(
+          host = self$host,
+          port = self$port,
+          blocking = TRUE,
+          server = self$server,
+          open = "r+b")
+    }
+  )
+)
+
+
+#' TServerSocket
+#'
+#' Socket implementation for server side.
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @format An \code{\link{R6Class}} generator object
+#'
+#' @export
+TServerSocket <- R6Class("TServerSocket",
+  public = list(
+    host = NA,
+    port = NA,
+    sock = NA,
+    initialize = function(
+        host = NA,
+        port = NA) {
+      self$host <- host
+      self$port <- port
+    },
+    listen = function() {
+      self$sock <- TSocket$new(self$host, self$port, TRUE)
+    },
+    accept = function() {
+      self$sock$open()
+      return(self$sock)
+    }
+  )
+)
---
+++ tests/testthat/test.test_socket.R
@@ -0,0 +1,38 @@
+#! /usr/bin/env Rscript
+
+library(testthat)
+library(parallel)
+
+context("socket")
+
+test_socket <- function(server_socket, client_socket) {
+
+  server <- function() {
+    server_socket$listen()
+    conn <- server_socket$accept()
+    buff3 <- conn$read(12)
+    conn$write(buff3)
+    conn$close()
+  }
+
+  client <- function () {
+    client_socket$open()
+    buff <- charToRaw("Hello World!")
+    client_socket$write(buff)
+    buff2 <- client_socket$read(12)
+    client_socket$close()
+    all(buff == buff2)
+  }
+  expect_true(mclapply(list(server, client), function(x) {
+    x()
+  }, mc.cores=2)[[2]])
+}
+
+test_that("test_inet_socket", {
+  if(.Platform$OS.type == "unix") {
+    server_socket <- thriftr::TServerSocket$new(host="localhost", port=6011)
+    client_socket <- thriftr::TSocket$new(host="localhost", port=6011)
+
+    test_socket(server_socket, client_socket)
+  }
+})
