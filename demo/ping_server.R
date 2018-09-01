361ed2a4f640496bd8fa52f4ec9c3e7ca6ae5baa
Marek Jagielski <marek.jagielski@gmail.com>
2018-09-01 21:57:43 +0200

    add server examples

---
+++ demo/ping_server.R
@@ -0,0 +1,22 @@
+#! /usr/bin/env Rscript
+
+pp_thrift <- thriftr::load("ping/pingpong.thrift", module_name="pp_thrift")
+
+Dispatcher <- R6::R6Class("Dispatcher",
+  public = list(
+    ping = function() {
+      print("ping pong!")
+      return("pong")
+    }
+  )
+)
+
+server <- thriftr::make_server(
+    pp_thrift$PingService,
+    Dispatcher$new(),
+    "127.0.0.1",
+    6000)
+
+print("serving...")
+
+server$serve()
---
+++ demo/sleep_server.R
@@ -0,0 +1,23 @@
+#! /usr/bin/env Rscript
+
+sleep_thrift <- thriftr::load("oneway/sleep.thrift", module_name="sleep_thrift")
+
+Dispatcher <- R6::R6Class("Dispatcher",
+  public = list(
+  sleep = function(seconds) {
+      print(sprintf("I'm going to sleep %d seconds", seconds))
+      Sys.sleep(seconds)
+      print("Sleep over!")
+    }
+  )
+)
+
+server <- thriftr::make_server(
+    sleep_thrift$Sleep,
+    Dispatcher$new(),
+    "127.0.0.1",
+    6000)
+
+print("serving...")
+
+server$serve()
